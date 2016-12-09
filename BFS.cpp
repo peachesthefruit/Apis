8d4ed24b882579d7b0a044e024c47a17e5bc8fe1
Shelby Lem <slem@nd.edu>
2016-12-08 20:50:28 -0500

    added getopts to BFS

--- BFS.cpp
+++ BFS.cpp
@@ -14,17 +14,26 @@ address
 #include <climits>
 #include <random>
 #include <string>
+#include <unistd.h>
+
+//Global Variables
+
 
 typedef std::map<std::string, std::vector<std::string>>::iterator it;
+int n = 5;
+int s = 100;
+std::string baddr = "cnn.com";
+std::string raddr = "cnn.com";
+bool b = false;
+bool r = false;
 
 class Graph{
 	public:
 		Graph();
-		void BFS(std::string, int); // rethink this
-		void randomWalk(std::string, int);
-		void printSuggest(std::map<std::string,int>);
+		void BFS();
+		void randomWalk();
+		void printSuggest(std::map<std::string, int>);
 		int get_random(int);
-//		int degrees = 3;
 
 	private:
 		std::map<std::string,std::vector<std::string>> graph;
@@ -40,9 +49,9 @@ Graph::Graph(){
 	}
 }
 
-void Graph::BFS(std::string address, int levels){
+void Graph::BFS(){
 	std::queue<it> q;
-	it addr = graph.find(address);
+	it addr = graph.find(baddr);
 	q.push(addr);
 	int prev = 1;
 	int next = 0;
@@ -62,9 +71,9 @@ void Graph::BFS(std::string address, int levels){
 		if(prev == 0){
 			prev = next;
 			next = 0;
-			levels--;
+			n--;
 		}
-		if(levels < 0){
+		if(n < 0){
 			printSuggest(suggest);
 			break;
 		}
@@ -76,15 +85,22 @@ void Graph::BFS(std::string address, int levels){
 	}
 }
 
-void Graph::randomWalk(std::string address, int degrees){
+void Graph::randomWalk(){
 	std::map<std::string, int> rand;
-	it addr = graph.find(address);
+	it addr = graph.find(raddr);
 	if (addr == graph.end()){
 		return;
 	}
-	for(int i = 0; i < degrees; i++){
-		size_t n = addr->second.size();
-		std::string temp = addr->second[get_random(n)];
+	std::string previous = raddr;
+	for(int i = 0; i < s; i++){
+		size_t num = addr->second.size();
+		std::string temp;
+		if(num == 0){
+			temp = previous;
+		}
+		else{
+			temp = addr->second[get_random(num)];
+		}
 		it next = graph.find(temp);
 		std::cout <<temp<<std::endl;
 		if(next != addr){
@@ -96,6 +112,7 @@ void Graph::randomWalk(std::string address, int degrees){
 				rand[temp] = 1;
 			}
 		}
+		previous = addr->first;
 		addr = next;
 	}
 	std::cout<<"------------"<<std::endl;
@@ -108,16 +125,58 @@ void Graph::printSuggest(std::map<std::string,int> s){
 	}
 }
 
-int Graph::get_random(int n){
+int Graph::get_random(int size){
 	std::random_device rd;
 	std:: default_random_engine g(rd());
-	std::uniform_int_distribution<int> d(0, n-1);
+	std::uniform_int_distribution<int> d(0, size-1);
 	return d(g);
 }
 
-int main(){
+//Non-class functions
+void usage(int status){
+	std::cout<<"usage: honeybee.cpp"<<std::endl;
+	std::cout<<"	-b BADDR	BST for the address BADDR"<<std::endl;
+	std::cout<<"	-r RADDR	Run random walk for the address RADDR"<<std::endl;
+	std::cout<<"	-n N		number of levels to traverse for BST"<<std::endl;
+	std::cout<<"	-s S		number of steps to take when random walking"<<std::endl;
+}
+
+void parse(int argc, char *argv[]){
+	int c;
+	while ((c = getopt(argc, argv, "hb:r:n:s:")) != -1){
+		switch (c){
+			case 'b':
+				b = true;
+				baddr = optarg;
+				break;
+			case 'r':
+				r = true;
+				raddr = optarg;
+				break;
+			case 'n':
+				n = std::atoi(optarg);
+				break;
+			case 's':
+				s = std::atoi(optarg);
+				break;
+			case 'h':
+				usage(0);
+				break;
+			default:
+				usage(1);
+				break;
+		}
+	}
+}
+
+int main(int argc, char *argv[]){
+	parse(argc, argv);
 	Graph myGraph;
-	//myGraph.BFS("swagmaster",4);
-	myGraph.randomWalk("cnn", 10);
+	if(b){
+		myGraph.BFS();
+	}
+	if(r){
+		myGraph.randomWalk();
+	}
 	return 0;
 }
